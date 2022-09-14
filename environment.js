dfba57e3ae5ada21ca3edd379705c2cb199dc9e9
adrian.ganga <adiganga2002@gmail.com>
2022-09-14 13:32:23 +0300

    Add terms and privacy + onboarding + css adjusments

--- environment.js
+++ environment.js
@@ -1,5 +1,6 @@
 export default {
   "epiDomain": "demo.epi",
   "leafletWebApiUrl": "https://admin.pla.health/leaflets",
-  "epiProtocolVersion": 1
+  "epiProtocolVersion": 1,
+  "appBuildVersion": "v1.5.4"
 }
