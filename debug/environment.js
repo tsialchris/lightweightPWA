f5b83754b862c81952c02110c4ffb700f1e7a046
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2022-12-06 17:09:00 +0200

    Update environment.js

--- debug/environment.js
+++ debug/environment.js
@@ -1,6 +1,6 @@
 export default {
   "epiDomain": "demo.epi",
   "epiProtocolVersion": 1,
-  "appBuildVersion": "1.6.0",
+  "appBuildVersion": "1.6.0-camera-test",
   "bdnsUrl": "https://raw.githubusercontent.com/PharmaLedger-IMI/mobile-bdns/master/bdns.json"
 }
