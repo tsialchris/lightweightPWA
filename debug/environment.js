f0c4d4dea3003e170b4a10e320983ea7044cf255
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2022-12-07 16:00:26 +0200

    debug env fix

--- debug/environment.js
+++ debug/environment.js
@@ -1,5 +1,5 @@
 export default {
-  "epiDomain": "localhost.epi",
+  "epiDomain": "demo.epi",
   "epiProtocolVersion": 1,
   "appBuildVersion": "1.6.0-cu-sortare2",
   "bdnsUrl": "https://raw.githubusercontent.com/PharmaLedger-IMI/mobile-bdns/master/bdns.json"
