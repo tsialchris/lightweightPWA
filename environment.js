c9467f0aa9e19ae4701ae199d9fa913dc8a5765f
adrian.ganga <adiganga2002@gmail.com>
2023-01-26 16:00:04 +0200

    EPI-202 New message error in LPWA when the remote system is busy

--- environment.js
+++ environment.js
@@ -1,6 +1,6 @@
 export default {
   "epiDomain": "demo.epi",
   "epiProtocolVersion": 1,
-  "appBuildVersion": "2023.0.2.a",
+  "appBuildVersion": "2023.0.2.patch3",
   "bdnsUrl": "https://raw.githubusercontent.com/PharmaLedger-IMI/mobile-bdns/master/bdns.json"
 }
