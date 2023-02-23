167b3587da74d335d3dbd514e9528454efd69b5c
adrian.ganga <adiganga2002@gmail.com>
2023-02-23 17:34:06 +0200

    Update for 2023.0.4_rc10 version

--- environment.js
+++ environment.js
@@ -1,6 +1,6 @@
 export default {
   "epiDomain": "demo.epi",
   "epiProtocolVersion": 1,
-  "appBuildVersion": "2023.0.4_rc6",
+  "appBuildVersion": "2023.0.4_rc10",
   "bdnsUrl": "https://raw.githubusercontent.com/PharmaLedger-IMI/mobile-bdns/master/bdns.json"
 }
