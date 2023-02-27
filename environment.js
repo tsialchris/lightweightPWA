c6c9ecacaacdbee6591996d5dc6ef226d0597cfe
adrian.ganga <adiganga2002@gmail.com>
2023-02-27 13:03:48 +0200

    Update for 2023.0.4_rc12_d increased timout for gtinOwner

--- environment.js
+++ environment.js
@@ -1,6 +1,6 @@
 export default {
   "epiDomain": "demo.epi",
   "epiProtocolVersion": 1,
-  "appBuildVersion": "2023.0.4_rc12_c",
+  "appBuildVersion": "2023.0.4_rc12_d",
   "bdnsUrl": "https://raw.githubusercontent.com/PharmaLedger-IMI/mobile-bdns/master/bdns.json"
 }
