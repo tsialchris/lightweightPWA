7f375fe8b6cc3a8fd437cd475d2f151b15cf69aa
adrian.ganga <adiganga2002@gmail.com>
2023-05-24 12:56:41 +0300

    test findings version 1.1

--- debug/environment.js
+++ debug/environment.js
@@ -1,6 +1,6 @@
 export default {
   "epiDomain": "local.epi",
   "epiProtocolVersion": 1,
-  "appBuildVersion": "Accessibility 1.0",
+  "appBuildVersion": "Accessibility 1.1",
   "bdnsUrl": "https://raw.githubusercontent.com/PharmaLedger-IMI/mobile-bdns/master/bdns.json"
 }
