39098ce7b1ef853ad581c0bf24a812a63e3e04af
adrian.ganga <adiganga2002@gmail.com>
2023-05-26 09:58:01 +0300

    fix loader issues

--- debug/environment.js
+++ debug/environment.js
@@ -1,6 +1,6 @@
 export default {
   "epiDomain": "local.epi",
   "epiProtocolVersion": 1,
-  "appBuildVersion": "Accessibility 1.2",
+  "appBuildVersion": "Accessibility 1.3",
   "bdnsUrl": "https://raw.githubusercontent.com/PharmaLedger-IMI/mobile-bdns/master/bdns.json"
 }
