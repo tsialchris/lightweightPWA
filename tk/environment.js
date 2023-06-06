748933d4b2943aaaf72218d19dd6ea5aa90f1a11
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2023-06-06 15:17:32 +0300

    Update environment.js

--- tk/environment.js
+++ tk/environment.js
@@ -2,5 +2,5 @@ export default {
   "epiDomain": "preqa.epi",
   "epiProtocolVersion": 1,
   "appBuildVersion": "2023.2.0-rc6",
-  "bdnsUrl": "https://raw.githubusercontent.com/PharmaLedger-IMI/mobile-bdns/master/bdns.json"
+  "bdnsUrl": "https://raw.githubusercontent.com/axiologic-pla/mobile-bdns/master/bdns.json"
 }
