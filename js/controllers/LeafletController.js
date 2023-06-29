cce6a141ffbf0f68a9bfbe0e05e20a96544ab164
adrian.ganga <adiganga2002@gmail.com>
2023-06-29 11:35:48 +0300

    Fix leaflet chapters alignment

--- js/controllers/LeafletController.js
+++ js/controllers/LeafletController.js
@@ -159,9 +159,15 @@ function LeafletController() {
       document.querySelector(".proceed-button.no-leaflet").setAttribute('style', 'display:none');
       //  document.querySelector(".text-section.no-leaflet").setAttribute('style', 'display:none');
       let languagesContainer = document.querySelector(".languages-container");
+      /*
+        site for flags https://flagpedia.net/download
+      */
       result.availableLanguages.forEach((lang, index) => {
         let langRadio = `<div class="flag-label-wrapper">
-        <label for="${lang.value}"> <img src="./images/flags/${lang.value}.svg" class="language-flag"/> ${lang.label} - (${lang.nativeName})</label> </div><input type="radio" name="languages" ${index === 0 ? "checked" : ""} value="${lang.value}" id="${lang.value}">`;
+        <label for="${lang.value}"> 
+          <img src="./images/flags/${lang.value}.svg" class="language-flag"/> ${lang.label} - (${lang.nativeName})
+        </label> 
+        </div><input type="radio" name="languages" ${index === 0 ? "checked" : ""} value="${lang.value}" id="${lang.value}">`;
         let radioFragment = document.createElement('div');
         radioFragment.classList.add("language-item-container");
         radioFragment.innerHTML = langRadio;
