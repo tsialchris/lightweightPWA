4783019e7ccd78156b11bbda9e45a15712a95b30
adrian.ganga <adiganga2002@gmail.com>
2023-06-29 21:33:57 +0300

    Fix for errorcode 99 vs 011

--- environment.js
+++ environment.js
@@ -1,7 +1,7 @@
 export default {
   "epiDomain": "demo.epi",
   "epiProtocolVersion": 1,
-  "appBuildVersion": "2023.2.0-rc15b",
+  "appBuildVersion": "2023.2.0-rc15c",
   "timePerCall": 10000,
   "totalWaitTime": 60000,
   "gto_TimePerCall": 3000,
