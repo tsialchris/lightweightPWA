df5f73b11e36d3667303432fa5972ecbd33da338
adrian.ganga <adiganga2002@gmail.com>
2023-06-29 10:45:24 +0300

    Fix for enableDomain

--- environment.js
+++ environment.js
@@ -1,10 +1,11 @@
 export default {
   "epiDomain": "demo.epi",
   "epiProtocolVersion": 1,
-  "appBuildVersion": "2023.2.0-rc15",
+  "appBuildVersion": "2023.2.0-rc15a",
   "timePerCall": 10000,
   "totalWaitTime": 60000,
   "gto_TimePerCall": 3000,
   "gto_TotalWaitTime": 15000,
+  "enableEpiDomain": false,
   "bdnsUrl": "https://raw.githubusercontent.com/PharmaLedger-IMI/mobile-bdns/master/bdns.json"
 }
--- js/controllers/LeafletController.js
+++ js/controllers/LeafletController.js
@@ -22,7 +22,7 @@ function LeafletController() {
     let gtin = urlParams.get("gtin");
     let batch = urlParams.get("batch");
     let expiry = urlParams.get("expiry");
-    let lsEpiDomain = localStorage.getItem(constants.EPI_DOMAIN);
+    let lsEpiDomain = environment.enableEpiDomain ? localStorage.getItem(constants.EPI_DOMAIN) : environment.epiDomain;
     lsEpiDomain = lsEpiDomain || environment.epiDomain;
     let timePerCall = environment.timePerCall || 10000;
     let totalWaitTime = environment.totalWaitTime || 60000;
@@ -160,7 +160,11 @@ function LeafletController() {
       //  document.querySelector(".text-section.no-leaflet").setAttribute('style', 'display:none');
       let languagesContainer = document.querySelector(".languages-container");
       result.availableLanguages.forEach((lang, index) => {
-        let langRadio = `<div class="flag-label-wrapper"><img src="./images/flags/${lang.value}.png" class="language-flag"/><span for="${lang.value}"> ${lang.label} - (${lang.nativeName})</span> </div><input type="radio" name="languages" ${index === 0 ? "checked" : ""} value="${lang.value}" id="${lang.value}">`;
+        let langRadio = `<div class="flag-label-wrapper">
+        /*
+        site for flags https://flagpedia.net/download
+        */
+        <label for="${lang.value}"> <img src="./images/flags/${lang.value}.svg" class="language-flag"/> ${lang.label} - (${lang.nativeName})</label> </div><input type="radio" name="languages" ${index === 0 ? "checked" : ""} value="${lang.value}" id="${lang.value}">`;
         let radioFragment = document.createElement('div');
         radioFragment.classList.add("language-item-container");
         radioFragment.innerHTML = langRadio;
@@ -190,10 +194,9 @@ function LeafletController() {
 
 }
 
-
 document.querySelector(".loader-container").setAttribute('style', 'display:block');
 const leafletController = new LeafletController();
-window.history.replaceState({prevPage: "leaflet"},"","index.html");
+
 
 window.leafletController = leafletController;
 
