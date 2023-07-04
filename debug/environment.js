28253a45afa10727d5de7d451a3aece10fc88220
adrian.ganga <adiganga2002@gmail.com>
2023-07-04 13:09:03 +0300

    update debug test

--- debug/environment.js
+++ debug/environment.js
@@ -1,7 +1,7 @@
 export default {
   "epiDomain": "demo.epi",
   "epiProtocolVersion": 1,
-  "appBuildVersion": "test_about1",
+  "appBuildVersion": "test_about2",
   "timePerCall": 10000,
   "totalWaitTime": 60000,
   "gto_TimePerCall": 3000,
