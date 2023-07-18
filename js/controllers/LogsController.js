fb5f26be2d61115c6a907029f99cda57cb087647
adrian.ganga <adiganga2002@gmail.com>
2023-07-18 13:46:42 +0300

    add downlaod and share to logs page

--- js/controllers/LogsController.js
+++ js/controllers/LogsController.js
@@ -9,9 +9,11 @@ function LogsController() {
     text: formattedJSON
   };
 
+  const copyBtn = document.querySelector("button#copy-btn");
   const shareBtn = document.querySelector("button#share-btn");
+  const downloadBtn = document.querySelector("button#download-btn");
 
-  shareBtn.addEventListener("click", async () => {
+  copyBtn.addEventListener("click", async () => {
     try {
       await navigator.clipboard.writeText(formattedJSON);
       alert("Copied to clipboard");
@@ -19,6 +21,26 @@ function LogsController() {
       alert(`Error: ${err}`);
     }
   });
+
+  shareBtn.addEventListener("click", async () => {
+    try {
+      await navigator.share(shareData);
+      alert("Data shared successfully");
+    } catch (err) {
+      alert(`Error: ${err}`);
+    }
+  });
+
+
+  downloadBtn.addEventListener("click", async () => {
+    let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(formattedJSON);
+    let downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute("download", "lwa-logs.json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+  })
 }
 
 const logsController = new LogsController();
