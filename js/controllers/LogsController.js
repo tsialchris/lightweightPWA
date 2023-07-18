520c276a95cb0be7a3994a0d2db36c1de9dabbaa
adrian.ganga <adiganga2002@gmail.com>
2023-07-18 13:10:36 +0300

    add copy to clipboard to logs page

--- js/controllers/LogsController.js
+++ js/controllers/LogsController.js
@@ -1,9 +1,24 @@
 import constants from "../constants.js";
 
 function LogsController() {
+
   let debugLog = JSON.parse(localStorage.getItem(constants.DEV_DEBUG));
   const formattedJSON = JSON.stringify(debugLog, null, 4);
   document.querySelector("#logs-container").innerHTML = formattedJSON;
+  const shareData = {
+    text: formattedJSON
+  };
+
+  const shareBtn = document.querySelector("button#share-btn");
+
+  shareBtn.addEventListener("click", async () => {
+    try {
+      await navigator.clipboard.writeText(formattedJSON);
+      alert("Copied to clipboard");
+    } catch (err) {
+      alert(`Error: ${err}`);
+    }
+  });
 }
 
 const logsController = new LogsController();
