dcbcc7b2e49b29eb529179451ec5ce6726e2642f
adrian.ganga <adiganga2002@gmail.com>
2023-07-26 10:19:01 +0300

    adjust css to prepare release 1.3

--- debug/js/utils/LightSmartUrl.js
+++ debug/js/utils/LightSmartUrl.js
@@ -1,23 +1,31 @@
 function SmartUrl(bdnsEntry){
+    if(bdnsEntry.concatWith){
+        //bdnsEntry is already a SmartUrl instance
+        return bdnsEntry;
+    }
+
     let url = typeof bdnsEntry === "string" ? bdnsEntry : bdnsEntry.url;
 
     function getOptions(options){
-        let opts = options || {};
+        let opts = options || this.opts || {};
         if(!opts.headers){
             opts.headers = {};
         }
         if(url !== bdnsEntry && bdnsEntry.headers){
             Object.assign(opts.headers, bdnsEntry.headers);
         }
+        this.opts = opts;
         return opts;
     }
 
     this.fetch = (options)=>{
-        return fetch(url, getOptions(options));
+        return fetch(url, getOptions.call(this, options));
     }
 
     this.getRequest = (options)=>{
-        return new Request(url, getOptions(options));
+        let request = new Request(url, getOptions.call(this, options));
+        request.smartUrl = this;
+        return request;
     }
 
     function concatUrls(base, path){
