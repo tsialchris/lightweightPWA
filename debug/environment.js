29f241f30fd86d7695347a2f9c0e0147279ccf33
adrian.ganga <adiganga2002@gmail.com>
2023-08-02 11:35:04 +0300

    adjust css to prepare release 1.3i

--- debug/environment.js
+++ debug/environment.js
@@ -1,7 +1,7 @@
 export default {
   "epiDomain": "demo.epi",
   "epiProtocolVersion": 1,
-  "appBuildVersion": "test_release.1.3g",
+  "appBuildVersion": "test_release.1.3i",
   "timePerCall": 10000,
   "totalWaitTime": 60000,
   "gto_TimePerCall": 3000,
