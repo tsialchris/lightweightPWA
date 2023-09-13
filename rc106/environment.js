1eff40505b30f96a190f9bd7eb2f1dc688981b1a
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2023-09-13 08:30:21 +0300

    rc106

---
+++ rc106/environment.js
@@ -0,0 +1,11 @@
+export default {
+  "epiDomain": "preqa.epi",
+  "epiProtocolVersion": 1,
+  "appBuildVersion": "2023.2.0-rc106",
+  "timePerCall": 10000,
+  "totalWaitTime": 60000,
+  "gto_TimePerCall": 3000,
+  "gto_TotalWaitTime": 15000,
+  "enableEpiDomain": true,
+  "bdnsUrl": "https://raw.githubusercontent.com/PharmaLedger-IMI/mobile-bdns/master/bdns.json"
+}
