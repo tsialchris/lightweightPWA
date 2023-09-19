e4df03b83e2af20b6f0ece34cd5c95663950a365
adrian.ganga <adiganga2002@gmail.com>
2023-09-19 11:39:44 +0300

    sync with dev

--- debug/lib/zxing-wrapper/scanner.js
+++ debug/lib/zxing-wrapper/scanner.js
@@ -279,7 +279,8 @@ export default function Scanner(domElement, testMode) {
 			canvas = document.createElement("canvas");
 			canvas.id = id;
 
-			context = canvas.getContext("2d");
+			context = canvas.getContext("2d",{willReadFrequently:true});
+
 			context.imageSmoothingEnabled = false;
 
 			canvas.setAttribute("style", "position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);");
--- debug/local_environment.js
+++ debug/local_environment.js
@@ -1,11 +1,11 @@
 export default {
-  "epiDomain": "localhost.epi",
+  "epiDomain": "local.epi",
   "epiProtocolVersion": 1,
-  "appBuildVersion":  "2023.x.x",
+  "appBuildVersion": "2023.0.4",
   "timePerCall": 10000,
   "totalWaitTime": 60000,
   "gto_TimePerCall": 3000,
   "gto_TotalWaitTime": 15000,
   "enableEpiDomain": true,
-  "bdnsUrl": "https://raw.githubusercontent.com/pharmaledgerassoc/mobile-bdns/master/bdns.json"
+  "bdnsUrl": "http://127.0.0.1:8080/bdns"
 }
