8c1d6bb1cbf7e13c4ad4ff7304f97ecf45f0c019
adrian.ganga <adiganga2002@gmail.com>
2023-09-25 10:20:36 +0300

    fix for leaflet zoom

--- debug/environment.js
+++ debug/environment.js
@@ -1,7 +1,7 @@
 export default {
   "epiDomain": "preqa.epi",
   "epiProtocolVersion": 1,
-  "appBuildVersion": "2023.3.0_zoom",
+  "appBuildVersion": "2023.3.0_zoom_a",
   "timePerCall": 10000,
   "totalWaitTime": 60000,
   "gto_TimePerCall": 3000,
