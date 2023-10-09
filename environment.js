c5a2520db019230d810d37eb17c66b1c7035dec2
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2023-10-09 08:28:22 +0300

    Update environment.js

--- environment.js
+++ environment.js
@@ -6,6 +6,6 @@ export default {
   "totalWaitTime": 60000,
   "gto_TimePerCall": 3000,
   "gto_TotalWaitTime": 15000,
-  "enableEpiDomain": true,
+  "enableEpiDomain": false,
   "bdnsUrl": "https://raw.githubusercontent.com/PharmaLedger-IMI/mobile-bdns/master/bdns.json"
 }
