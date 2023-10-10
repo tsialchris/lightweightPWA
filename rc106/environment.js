5de8165c85d833a3bf5255777dc967f6c206e9e1
adrian.ganga <adiganga2002@gmail.com>
2023-10-10 15:00:55 +0300

    v1 for html tag

--- rc106/environment.js
+++ rc106/environment.js
@@ -1,7 +1,7 @@
 export default {
   "epiDomain": "preqa.epi",
   "epiProtocolVersion": 1,
-  "appBuildVersion": "2023.2.0-rc106",
+  "appBuildVersion": "2023.2.0-rc106_a",
   "timePerCall": 10000,
   "totalWaitTime": 60000,
   "gto_TimePerCall": 3000,
