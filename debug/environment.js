e733dbb128bab4b1e5ccbb76002e5433c97d6d1b
adrian.ganga <adiganga2002@gmail.com>
2023-10-25 11:16:50 +0300

    test camera overlay

--- debug/environment.js
+++ debug/environment.js
@@ -1,7 +1,7 @@
 export default {
   "epiDomain": "preqa.epi",
   "epiProtocolVersion": 1,
-  "appBuildVersion": "test_overlay_5",
+  "appBuildVersion": "test_overlay_6",
   "timePerCall": 10000,
   "totalWaitTime": 60000,
   "gto_TimePerCall": 3000,
