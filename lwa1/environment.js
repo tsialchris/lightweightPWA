abb30ad7104f60888e28e868bb6c4351aef26dd2
adrian.ganga <adiganga2002@gmail.com>
2023-11-10 15:54:09 +0200

    automation test

---
+++ lwa1/environment.js
@@ -0,0 +1,11 @@
+export default {
+  "epiDomain": "preqa.epi",
+  "epiProtocolVersion": 1,
+  "appBuildVersion": "LWA2.0.1",
+  "timePerCall": 10000,
+  "totalWaitTime": 60000,
+  "gto_TimePerCall": 3000,
+  "gto_TotalWaitTime": 15000,
+  "enableEpiDomain": true,
+  "bdnsUrl": "https://raw.githubusercontent.com/PharmaLedger-IMI/mobile-bdns/master/bdns.json"
+}
