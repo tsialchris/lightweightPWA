c5aaf9a117e78759a8fb5d20f060c124eb1eee9b
adrian.ganga <adiganga2002@gmail.com>
2024-01-10 21:39:41 +0200

    test new font size

--- lwa-tst/app/js/controllers/MainController.js
+++ lwa-tst/app/js/controllers/MainController.js
@@ -160,13 +160,13 @@ function MainController() {
 
 const mainController = new MainController();
 
-window.onload = async (event) => {
+document.addEventListener('DOMContentLoaded', async (event) => {
   await translate();
   mainController.checkOnboarding();
   document.querySelector(".page-container").classList.remove("hiddenElement");
   document.querySelector(".loader-container").setAttribute('style', 'display:none');
   setFontSize();
-}
+})
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 let epiDomain = urlParams.get("setdomain") || localStorage.getItem(constants.EPI_DOMAIN) || environment.epiDomain;
