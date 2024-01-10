c4e554c58ab379fa1db40e738dddb82fa8744978
adrian.ganga <adiganga2002@gmail.com>
2024-01-11 00:20:19 +0200

    test new font size

--- lwa-tst/utils.js
+++ lwa-tst/utils.js
@@ -283,12 +283,12 @@ function saveFontZoom() {
 function zoomFont(scaleFactor, ignoreBrowser) {
   console.log("zoom = ", scaleFactor);
 
-  if (scaleFactor === "100") {
+  if(scaleFactor === "100"){
     return
   }
   let visualViewportDelta = window.visualViewport.scale;// > 2 ? window.visualViewport.scale / 2 : 1
   let currentBrowser = ignoreBrowser ? "safari" : getBrowser();
-  document.documentElement.style.setProperty('--font-size--basic', constants.FONT_SCALE_MAP.basic_font[scaleFactor][currentBrowser]);
+  document.documentElement.style.setProperty('--font-size--basic', constants.FONT_SCALE_MAP.basic_font[scaleFactor][currentBrowser] );
   document.documentElement.style.setProperty('--font-size--M', constants.FONT_SCALE_MAP.m_font[scaleFactor][currentBrowser]);
   document.documentElement.style.setProperty('--font-size--L', constants.FONT_SCALE_MAP.l_font[scaleFactor][currentBrowser]);
   document.documentElement.style.setProperty('--font-size--XL', constants.FONT_SCALE_MAP.xl_font[scaleFactor][currentBrowser]);
