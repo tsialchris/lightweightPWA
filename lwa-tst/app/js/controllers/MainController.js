fbe912603c70eed57c1af56aa0d69ca39d47503c
adrian.ganga <adiganga2002@gmail.com>
2024-01-10 15:11:05 +0200

    test new font size

--- lwa-tst/app/js/controllers/MainController.js
+++ lwa-tst/app/js/controllers/MainController.js
@@ -41,15 +41,13 @@ function MainController() {
   this.checkOnboarding = function () {
     let usrAgreedTerms = getCookie("usr_agreed_terms");
     if (!usrAgreedTerms) {
-      let welcomeText = getTranslation("onboarding_welcome");
-      document.querySelector(".welcome-container h1").innerHTML = welcomeText;
+      document.querySelector(".welcome-container #onbording-text").classList.remove("hiddenElement")
       document.querySelector(".content-container").classList.add("hiddenElement");
       document.querySelector(".explain-container").classList.add("hiddenElement");
       document.querySelector(".scan-button-container").classList.add("hiddenElement");
     } else {
-      let welcomeText = getTranslation("welcome");
       document.querySelector(".terms-content-container").classList.add("hiddenElement");
-      document.querySelector(".welcome-container h1").innerHTML = welcomeText;
+      document.querySelector(".welcome-container #welcome-text").classList.remove("hiddenElement");
     }
     document.querySelector("#app_version_number").innerHTML = `${environment.appBuildVersion}`;
   }
