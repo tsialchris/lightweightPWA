6af25c45ca20dcb82feb44b733035615220f12ac
adrian.ganga <adiganga2002@gmail.com>
2024-01-17 15:11:11 +0200

    test new font size v5 (dialog)

--- lwa-tst/app/js/controllers/LeafletController.js
+++ lwa-tst/app/js/controllers/LeafletController.js
@@ -75,7 +75,7 @@ function LeafletController() {
     this.leafletLang = lang;
     getLeaflet(lang);
     setTextDirectionForLanguage(lang, "#leaflet-content");
-    setTextDirectionForLanguage(lang, ".modal-body .page-header");
+    setTextDirectionForLanguage(lang, ".modal-body .modal-header");
 
     document.querySelector("#leaflet-lang-select").classList.add("hiddenElement");
   }
